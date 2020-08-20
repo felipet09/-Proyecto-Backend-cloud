919cae9230ab440b48490f584f71a24b88556d06
Felipe Triana <w.trianag@uniandes.edu.co>
2020-08-19 22:11:44 -0500

    eliminar y crear

--- apps/eventos/urls.py
+++ apps/eventos/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import crearEvento,listarEvento
+from .views import crearEvento,listarEvento,editarEvento,eliminarEvento
 
 urlpatterns = [
     path('crear_evento/',crearEvento, name = 'crear_evento'),
-    path('listar_evento/',listarEvento, name = 'listar_evento')
+    path('listar_evento/',listarEvento, name = 'listar_evento'),
+    path('editar_evento/<int:id>',editarEvento, name = 'editar_evento'),
+    path('eliminar_evento/<int:id>',eliminarEvento, name = 'eliminar_evento')
 ]
--- apps/eventos/views.py
+++ apps/eventos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.exceptions import ObjectDoesNotExist
 from .forms import EventoForm
 from .models import Evento as get_evento
 
@@ -19,3 +20,27 @@ def crearEvento(request):
 def listarEvento(request):
     eventos = get_evento.objects.all()
     return render(request,'eventos/listar_evento.html',{'eventos':eventos})
+
+def editarEvento(request,id):
+    evento_form = None
+    error = None
+    try:
+        evento = get_evento.objects.get(id = id)
+        if request.method == 'GET':
+            evento_form = EventoForm(instance = evento)
+        else:
+            evento_form = EventoForm(request.POST, instance = evento)
+            if evento_form.is_valid():
+                evento_form.save()
+            return redirect('index')
+    except ObjectDoesNotExist as e:
+        error = e
+
+    return render(request,'eventos/crear_evento.html',{'evento_form':evento_form,'error':error})
+
+def eliminarEvento(request,id):
+    evento = get_evento.objects.get(id = id)
+    if request.method == 'POST':
+        evento.delete()
+        return redirect('eventos:listar_evento')
+    return render(request,'eventos/eliminar_evento.html',{'evento':evento})
